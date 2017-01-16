195d525cd3b9071402f1d4e64dccdba950270e81
Johan Andersson <joha@netcompany.com>
2017-01-16 14:00:00 +0100

    edit

--- greeter.js
+++ greeter.js
@@ -10,5 +10,5 @@ var Student = (function () {
 function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = new Student("Jane", "M.", "User");
+var user = new Student("Jane", "M.", "User222");
 document.body.innerHTML = greeter(user);
